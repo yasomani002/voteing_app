3b59c06c44962fb1f13ecb4056f466a698a91c18
Yash Somani <yashsomani231@gmail.com>
2025-08-12 21:19:40 +0530

    password decrypt

--- models/voterModule.js
+++ models/voterModule.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bycrypt = require('bcrypt');
+
 
 const voterSchema = new mongoose.Schema({
     name:{
@@ -29,5 +31,30 @@ const voterSchema = new mongoose.Schema({
     }
 })
 
+
+voterSchema.pre('save', async function(next) {
+    try {
+        const candidateData = this;
+        if(!candidateData.isModified('password')) return next();
+
+        // Hash the password before saving
+        const salt = await bycrypt.genSalt(10);
+        const hashpassword = await bycrypt.hash(candidateData.password,salt);
+        candidateData.password = hashpassword;
+        next();
+    } catch (error) {
+        next(error)       
+    }
+})
+
+
+voterSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+        const isMatch = await bycrypt.compare(candidatePassword,this.password)
+        return isMatch;
+    }catch(error){
+        throw new Error(error);
+    }
+}
 const Voter = mongoose.model('Voter',voterSchema)
 module.exports = Voter 
